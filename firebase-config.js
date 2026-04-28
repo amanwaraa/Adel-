175d63514b6823e85f146067a96ad8f2c35a970a
Maroshop <homworkhhh25@gmail.com>
2026-04-28 07:37:56 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.CASHIER_FIREBASE_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyCQmPVOYyJUGt4zvgoTTCgxLDCOqNRci2A",
+    authDomain: "adel-a3e78.firebaseapp.com",
+    databaseURL: "https://adel-a3e78-default-rtdb.firebaseio.com",
+    projectId: "adel-a3e78",
+    storageBucket: "adel-a3e78.firebasestorage.app",
+    messagingSenderId: "611825573524",
+    appId: "1:611825573524:web:8892dca23b5c8ec376de81",
+    measurementId: "G-DRZ5EKPL7R"
+  },
+
+  firebaseRoot: "cashier_units_pro_adel_885766842",
+
+  fallbackRoots: [
+    "cashier_units_pro_adel_885766842"
+  ]
+};
